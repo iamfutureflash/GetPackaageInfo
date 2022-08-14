420ebdb0c16506cc3b6f72a5ee101e6ac8664e18
iamfutureflash <xenode2000@gmail.com>
2022-08-15 00:36:32 +0530

    get package info

---
+++ app/src/main/java/com/flash/android/getpackagename/MainActivity.java
@@ -0,0 +1,19 @@
+package com.flash.android.getpackagename;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void ButtonGetInstalledAppList(View view){
+
+    }
+}
